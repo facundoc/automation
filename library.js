0aed5e8f26ac80353042bf2c614b3deb9f748671
Facundo Campi <facucampi@gmail.com>
2018-06-27 15:47:43 -0300

    added chai and chai-as-promised, tests are working

--- library.js
+++ library.js
@@ -1,67 +1,74 @@
-// try{
+try{
 var webdriver = require('selenium-webdriver'), 
-  // {
-  //   describe,
-  //   it,
-  //   after,
-  //   before
-  // } = require('selenium-webdriver/testing');
-By = webdriver.By,
-  until = webdriver.until;
-  var driver;
+    By = webdriver.By,
+    until = webdriver.until;
+
+
+var driver;
+var chai = require('chai');
+var expect = require('chai').expect;
+var should = chai.should();
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 
 describe("library app scenarios", function() {
       this.timeout(500000);
       beforeEach(function() {
         driver = new webdriver.Builder().forBrowser('chrome').build();
         driver.get("https://library-app.firebaseapp.com/");
-        // driver.wait(until.elementLocated(By.css(".page-footer")), 20000);
         console.log("before");
 
       });
+
       afterEach(function() {
         console.log("after");
         driver.quit();
       });
-      it.only('changes button opacity when email is valid', function(done) {
-        console.log("empezamos");
-        var isVisible;
-        // var submitBtn = driver.findElement(By.css('.btn-lg'));
-        console.log("variable creada");
-        
+      
+      it('changes button opacity when email is valid', function(done) {
+                
         driver.findElement(By.css('input')).sendKeys('user@mail.com');
-        console.log("buscado el input");
-  
+        
+        
         driver.findElement(By.css('.btn-lg')).then(function(result) {
             
-          console.log("entramos en el then");
-            return result.getCssValue('opacity').then(function (el){
-              isVisible = el == "1";
-              console.log("variable ", isVisible); 
-              if (isVisible){
-                return done()} 
-              else {
-                return done("el elemento no es visible")
-              };
+            
+            return result.getCssValue('opacity').should.eventually.equal('1').notify(done);
+            
+                                                 // .then(function (el){
+            //   // if (el == 1){
+            //   //   return done();
+            //   // } else {
+            //   //   return done("comparision failed");
+            //   // };
+            //   // assert.equal(el, 1,  "opacity equal to 1");              
+            //   // return done();
+            //   el.should.eventually.equal('1');
+            //   console.log("after assert");
               
-            });
+            //   done();
+            //   // return done();
 
-          });
-        
-          console.log("despues de opacity");
-          
+            // });
+
+          });          
           
       });
 
 
-      // it('works with mocha', function() {
-      //       driver.findElement(By.css('input')).sendKeys('usergmail.com');
-      //       driver.wait(until.elementLocated(By.css('.alert-success')), 20000).then(function(el) {
-      //         el.getText().then(function(txt) {
-      //           console.log(txt);
-      //         });
-      //       });
-      // });
+      it('get text from success', function(done) {
+            console.log("ALERTA TEST");
+            
+            driver.findElement(By.css('input')).sendKeys('user@gmail.com');
+            console.log("mando gmail");
+            driver.findElement(By.css(".btn-lg")).click();
+            
+            driver.wait(until.elementLocated(By.css('.alert-success')), 20000).then(function(el) {
+              console.log("entro en el primer then");
+              
+              el.getText().should.eventually.include("Thank you").notify(done);
+            });
+      });
 
       //         it('nav test', function() {
       //           driver.findElement(By.css('nav')).getText().then(function(txt) {
@@ -71,28 +78,7 @@ describe("library app scenarios", function() {
       //         });
 
 });
-  // } catch (error){
-  //   console.log(error);
-  // }
+  } catch (error){
+    console.log(error);
+  }
 
-
-
-
-            // // var driver = new webdriver.Builder().forBrowser('chrome').build();
-            // // // var st = "test string";
-            // // // console.log("0");
-            // // driver.get("https://library-app.firebaseapp.com/");
-            // // driver.manage().timeouts().implicitlyWait(20000);
-            // // console.log("1");
-            // driver.findElement(By.css('input')).sendKeys('user@gmail.com');
-            // // console.log("2");
-            // // driver.findElement(By.css('.btn-lg')).click();
-            // // console.log("3");
-            // driver.wait(function(){
-            //
-            // })
-            // driver.wait(until.elementLocated(By.css('.alert-success')), 20000).then(function(el){
-            //    el.getText().then(function (txt) {
-            //         console.log(txt);
-            //     });
-            // } );
